60990422ebd9a863171db0e5c453ea5937f520ff
Gabriel Lima | Dev <ogabriellima1999@gmail.com>
2025-05-19 00:22:07 -0300

    Upload Project

---
+++ ContaBancaria/src/main/java/Enum/SituacaoContaBancaria.java
@@ -0,0 +1,8 @@
+package Enum;
+
+public enum SituacaoContaBancaria {
+
+	ATIVO, BLOQUEADA,ENCERRADA
+	
+	
+}
---
+++ ContaBancaria/src/main/java/Enum/TipoDeConta.java
@@ -0,0 +1,10 @@
+package Enum;
+
+public enum TipoDeConta {
+	
+	SALARIO,
+	CORRENTE,
+	POUPANCA,
+	ESPECIAL
+	
+}
